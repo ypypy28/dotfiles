a6bbe9ed97ce4d4ebd45b5534df14510b6fe36cf
Nikolai Dyachkov <ypypy28@gmail.com>
2022-06-28 15:04:19 +0300

    add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require 'wezterm';
+return {
+    font = wezterm.font("mononoki"),
+    -- font = wezterm.font("Hack"),
+    font_size = 14,
+    hide_tab_bar_if_only_one_tab = true,
+}
