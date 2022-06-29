5a3574a644b37e0d231412a3e7d58248ab054ace
Nikolai Dyachkov <ypypy28@gmail.com>
2022-06-29 14:58:50 +0300

    add color scheme

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,6 +3,11 @@ return {
     font = wezterm.font("mononoki"),
     -- font = wezterm.font("Hack"),
     font_size = 14,
+
+    -- color_scheme = "Breeze",
+    -- color_scheme = "Darkside",
+    color_scheme = "OceanicMaterial",
+
     hide_tab_bar_if_only_one_tab = true,
     window_padding = {
         left = 0,
