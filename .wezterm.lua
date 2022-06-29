cc873dff708ac5774ead0ba1175fd860b2f25b44
Nikolai Dyachkov <ypypy28@gmail.com>
2022-06-29 13:13:09 +0300

    turn off paddings

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,4 +4,10 @@ return {
     -- font = wezterm.font("Hack"),
     font_size = 14,
     hide_tab_bar_if_only_one_tab = true,
+    window_padding = {
+        left = 0,
+        right = 0,
+        top = 0,
+        bottom = 0,
+    }
 }
