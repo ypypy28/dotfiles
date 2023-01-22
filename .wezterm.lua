dbb17270e7285e8ad62ced9b1a5e335b0e8e461d
Nikolai Diachkov <ypypy28@gmail.com>
2023-01-22 21:40:40 +0300

    delete ram, don't need

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,8 +1,6 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
 
-local ram = "QAYIND4PWMSSR5XMGVQWKNESS33CWZGL"
-
 return {
     font = wezterm.font_with_fallback({
         "mononoki",
